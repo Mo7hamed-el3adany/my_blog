8e456b55c0eb9aa93e7f644816ae283e62a17e8e
Mohammed Elsadany <74606586+elsadany32@users.noreply.github.com>
2022-08-29 01:36:15 +0200

    Delete client directory

--- client/src/redux/types/profileType.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from '../../utils/TypeScript'
-
-export const GET_OTHER_INFO = "GET_OTHER_INFO"
-
-
-export interface IGetOtherInfoType {
-  type: typeof GET_OTHER_INFO,
-  payload: IUser
-}
--- client/src/redux/types/socketType.ts
+++
@@ -1,8 +0,0 @@
-import { Socket } from 'socket.io-client'
-
-export const SOCKET = "SOCKET"
-
-export interface ISocketType {
-  type: typeof SOCKET,
-  payload: Socket
-}
